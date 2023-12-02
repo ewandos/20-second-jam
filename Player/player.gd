7cbb1ef3ba530afb19495aea03e01768dc624f7e
Philip Ewert <developer@philipewert.com>
2023-12-02 17:22:51 +0100

    player can click to kills monsters

--- Player/player.gd
+++ Player/player.gd
@@ -1,5 +1,21 @@
-extends Node
+extends Sprite2D
 
-func _process(delta):
+@onready var damage_area_2d := $DamageArea2D as Area2D
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		kill_monster()
+
+func _process(_delta):
 	self.position = get_viewport().get_mouse_position()
 
+func _on_damage_area_2d_area_entered(area: Monster) -> void:
+	area.can_move = false
+
+func _on_damage_area_2d_area_exited(area: Area2D) -> void:
+	area.can_move = true
+
+func kill_monster():
+	global_scale *= 0.9 
+	for monster in damage_area_2d.get_overlapping_areas():
+		monster.queue_free()
